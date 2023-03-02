081b81b03fa3cd01939cd9187137d4898b2f1cb8
Colin Marshall <colin.marshall94@gmail.com>
2023-03-01 23:11:48 -0800

    updated style for about me page

--- src/main/pages/aboutMe.jsx
+++ src/main/pages/aboutMe.jsx
@@ -10,7 +10,7 @@ export default function About() {
   return (
     <main className="overflow-auto">
       <img className="mx-auto xs:mt-1 xs:max-h-40 sm:mt-2 sm:max-h-52 md: mt-3 md:max-h-64" src={portrait} alt="My Portrait" />
-      <h1>Hey there, my name is Colin!  I'm a <span className="font-bold">Full-Stack Web Developer</span> offering a range of solutions in <span className="font-bold">Design, API Management, Server Management, <span className="font-normal">and</span> Database Management</span>.  Here are some of my skills:</h1>
+      <h1 className="w-3/4 m-auto p-10">Hey there, my name is Colin!  I'm a <span className="font-bold">Full-Stack Web Developer</span> offering a range of solutions in <span className="font-bold">Design, API Management, Server Management, <span className="font-normal">and</span> Database Management</span>.  Here are some of my skills:</h1>
       <SkillCards/>
       {/* render subcomponent with logo for each topic (GRID?)*/}
       {/* HTML */}
--- src/main/pages/skillCards.jsx
+++ src/main/pages/skillCards.jsx
@@ -1,15 +1,22 @@
 import React from "react";
 // import image logos for all 12 things
-import {data} from '../../helpers/skillCardData'
+import { data } from "../../helpers/skillCardData";
 
 export default function SkillCards() {
   return (
     <ul className="container m-auto w-3/4 flex flex-wrap">
       {data.map((obj) => {
         return (
-          <li className="flex-skill-icon flex-col items-center" key={obj.id}>
-            <img className="w-auto h-32" src={obj.link} alt={obj.name} />
-            <p>{obj.name}</p>
+          <li
+            className="flex-skill-icon border border-black rounded-xl border-solid m-5"
+            key={obj.id}
+          >
+            <h1 className="font-bold p-2 text-center">{obj.name}</h1>
+            <img
+              className="w-auto m-auto p-2 h-32 "
+              src={obj.link}
+              alt={obj.name}
+            />
           </li>
         );
       })}
